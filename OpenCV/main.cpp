4ddadbb1b22f5cd73ad584d0d7bd874f2894ecac
levink <levinkostya@yandex.ru>
2014-07-04 15:59:39 +0400

    2 сцены в одном окне. Наладил отображение. Выводится реальный FPS. Продолжаем рефакторинг

--- OpenCV/main.cpp
+++ OpenCV/main.cpp
@@ -5,7 +5,6 @@
 #include "camera.h"
 #include "mathdata.h"
 #include "glut.h"    
-#include "scene.h"
 
 #define TAN_30 0.57735026918962576450914878050196
 #define CTAN_30 1.7320508075688772935274463415059
@@ -13,9 +12,16 @@
 const double Z_NEAR = 3.0;
 const double Z_FAR = 13;
 
-int WINDOW_WIDTH = 320;  /*Разрешение окона OpenGL&CV по горизонтали*/ 
-int WINDOW_HEIGHT = 240; /*Разрешение окона OpenGL&CV по вертикали*/
-bool FullScreen = false;
+/* W_WIDTH - OpenGL&CV scene(!) horizontal size (not window) */ 
+/* W_HEIGHT - OpenGL&CV scene(!) vertical size */
+#define SCREEN_MODE 2
+#if SCREEN_MODE == 1
+	int W_WIDTH = 640;
+	int W_HEIGHT = 480;
+#elif SCREEN_MODE == 2
+	int W_WIDTH = 320;
+	int W_HEIGHT = 240;
+#endif
 
 
 double CAMERA_STEP = 0.4;
@@ -28,9 +34,9 @@ bool MoveForward = false,
 	 RotateLeft = false, 
 	 RotateRight = false;
 
-
+long prevTime = GetTickCount();
 int fps = 0;
-int w1 = 0, w2 = 0;
+int frameCount = 0;
 
 float arr[4] = {50.0, 10.0, 50.0, 1.0};
 float arr2[3] = {50.0, -1.0, 50.0};
@@ -59,219 +65,167 @@ void RestoreDepthFromBuffer(float* buf, int length)
 using namespace std;
 
 namespace SourceScene {
-	long t1 = 0;
 	int teapotAngle = 0;
-	 
-	
+	void DrawTeapots();
+	void DrawWalls();
+
 	void idle(void)
 	{
 		if (teapotAngle > 360) teapotAngle = 0;
 		else teapotAngle += 1;
 		glutPostRedisplay();
 	}
-	void reshape(int width, int height)
+	void reshape(int w, int h)
 	{
-		glViewport(0, 0, width, height);
-	
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		gluPerspective(60, ((double)width)/height, 1, 100);
+		gluPerspective(60, ((double)w)/h, 1, 100);
 
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 	}
 	void display(void)
 	{
+		glViewport(0, 0, W_WIDTH, W_HEIGHT);
+	
+		glPushMatrix();
+		glRotated(180, 0, 1, 0);
+		glRotated(w1camera.GetAngleXOZ(), 0, 1, 0);
+		glTranslated(-w1camera.GetX(), -w1camera.GetY(), -w1camera.GetZ());
+	
+		glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+	
+		GLfloat pos[4] = {25.0, 20.0, 25.0, 0.6};
+		GLfloat ambient[] = {0.2, 0.2, 0.2, 1.0}; 
+		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
+		glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+		glLightfv(GL_LIGHT0, GL_POSITION, pos);
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+		glEnable(GL_DEPTH_TEST);
 
-		/* Определение количества кадров в секунду (FPS)*/
-	//	long int t2 = GetTickCount(); 
-	//
-	//	if(t2-t1>10)
-	//	{
-	//		if(MoveForward)
-	//		{
-	//			w1camera.MoveForward(CAMERA_STEP);
-	//		}
-	//		if(MoveBack)
-	//		{
-	//			w1camera.MoveBack(CAMERA_STEP);
-	//		}
-	//		if(RotateLeft)
-	//		{
-	//			w1camera.Rotate(-CAMERA_STEP);
-	//		}
-	//		if(RotateRight)
-	//		{
-	//			w1camera.Rotate(CAMERA_STEP);
-	//		}
-	//		if(MoveUp)
-	//		{
-	//			w1camera.MoveUp(CAMERA_STEP);
-	//		}
-	//		if(MoveDown)
-	//		{
-	//			w1camera.MoveDown(CAMERA_STEP);
-	//		}
-	//	}
-	//
-	//
-	//	if(t2-t1>1000) 
-	//	{
-	//		fps = fpstmp;
-	//		fpstmp = 0;
-	//		t1=t2; 
-	//	} 
-	//	else fpstmp++;
-	//
-	//
-	//	
-	//
-	//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-	//
-	//	glEnable(GL_DEPTH_TEST);
-	//	GLfloat ambient[] = {0.2, 0.2, 0.2, 1.0}; 
-	//	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
-	//	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
-	//	glEnable(GL_LIGHTING);
-	//	
-	//	
-	//	
-	//	glRotated(180,0,1,0);
-	//	glRotated(w1camera.GetAngleXOZ(),0,1,0);
-	//	glTranslated(-w1camera.GetX(),-w1camera.GetY(), -w1camera.GetZ());
-	//
-	//	DrawTeapots();
-	//	DrawWalls();
-	//
-	////  img->origin = IPL_ORIGIN_BL;
-	////  gray->origin = IPL_ORIGIN_BL;
-	////	dst->origin = IPL_ORIGIN_BL;
-	//
-	////  glReadPixels(0,0, WINDOW_WIDTH, WINDOW_HEIGHT, GL_DEPTH_COMPONENT, GL_FLOAT, depth);
-	////	glReadPixels(0,0, WINDOW_WIDTH, WINDOW_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, img->imageData); 
-	//
-	//	
-	//
-	//	glFlush();
-	//	glutSwapBuffers();
+		DrawTeapots();
+		DrawWalls();
+
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_LIGHT0);
+		glDisable(GL_LIGHTING);
+
+		
+		glPopMatrix();
+		
+		//glReadPixels(0,0, W_WIDTH, W_HEIGHT, GL_DEPTH_COMPONENT, GL_FLOAT, depth);
+		//RestoreDepthFromBuffer(depth,1);
 	}
 	void keybord(unsigned char key, int x, int y)
 	{
-		if (key == 'j' || key == 238)
-		{
-			if(!FullScreen)
-			{
-				glutFullScreen();
-				FullScreen = true;
-			}
-			else
-			{
-				glutReshapeWindow(800,600);
-				FullScreen = false;
-			}
+		//if (key == 'j' || key == 238)
+		//{
+		//	if(!FullScreen)
+		//	{
+		//		glutFullScreen();
+		//		FullScreen = true;
+		//	}
+		//	else
+		//	{
+		//		glutReshapeWindow(800,600);
+		//		FullScreen = false;
+		//	}
 
-		}
+		//}
 
-		if (key == 'k' || key == 235)
-		{
-			cout << "[Окно 1] Состояние" << endl;
-			cout << "Текущие координаты: X: " << (int)w1camera.GetX() << ", Y: " << (int)w1camera.GetY() << ", Z: " << (int)w1camera.GetZ() << ", угол: " << (int)w1camera.GetAngleXOZ() << endl;
-			cout << "Разрешение окна: " << WINDOW_WIDTH << "x" << WINDOW_HEIGHT << endl;
-			cout << "Шаг камеры: " << CAMERA_STEP << endl;
-			cout << "FPS: " << fps << endl;
-			cout << endl;
-		}
+		//if (key == 'k' || key == 235)
+		//{
+		//	cout << "[Окно 1] Состояние" << endl;
+		//	cout << "Текущие координаты: X: " << (int)w1camera.GetX() << ", Y: " << (int)w1camera.GetY() << ", Z: " << (int)w1camera.GetZ() << ", угол: " << (int)w1camera.GetAngleXOZ() << endl;
+		//	cout << "Разрешение окна: " << W_WIDTH << "x" << W_HEIGHT << endl;
+		//	cout << "Шаг камеры: " << CAMERA_STEP << endl;
+		//	cout << "FPS: " << fps << endl;
+		//	cout << endl;
+		//}
 	
-		if (key == 'w' || key == 246) // Движение вперед
-		{
-			w1camera.MoveForward(CAMERA_STEP);
-			MoveForward = true;
-		}
+		//if (key == 'w' || key == 246) // Движение вперед
+		//{
+		//	w1camera.MoveForward(CAMERA_STEP);
+		//	MoveForward = true;
+		//}
 
-		if (key == 's' || key == 251) // Движение назад
-		{
-			w1camera.MoveBack(CAMERA_STEP);
-			MoveBack = true;
-		}
+		//if (key == 's' || key == 251) // Движение назад
+		//{
+		//	w1camera.MoveBack(CAMERA_STEP);
+		//	MoveBack = true;
+		//}
 
-		if (key == 'a' || key == 244) // Поворот камеры направо
-		{
-			w1camera.Rotate(-CAMERA_STEP);
-			RotateLeft = true;
-		}
+		//if (key == 'a' || key == 244) // Поворот камеры направо
+		//{
+		//	w1camera.Rotate(-CAMERA_STEP);
+		//	RotateLeft = true;
+		//}
 
-		if (key == 'd' || key == 226) // Поворот камеры налево
-		{
-			w1camera.Rotate(CAMERA_STEP);
-			RotateRight = true;
-		}
+		//if (key == 'd' || key == 226) // Поворот камеры налево
+		//{
+		//	w1camera.Rotate(CAMERA_STEP);
+		//	RotateRight = true;
+		//}
 
-		if (key == 'r' || key == 234) // Поворот камеры налево
-		{
-			w1camera.MoveUp(CAMERA_STEP);
-			MoveUp = true;
-		}
-		if (key == 'f' || key == 224) // Поворот камеры налево
-		{
-			w1camera.MoveDown(CAMERA_STEP);
-			MoveDown = true;
-		}
+		//if (key == 'r' || key == 234) // Поворот камеры налево
+		//{
+		//	w1camera.MoveUp(CAMERA_STEP);
+		//	MoveUp = true;
+		//}
+		//if (key == 'f' || key == 224) // Поворот камеры налево
+		//{
+		//	w1camera.MoveDown(CAMERA_STEP);
+		//	MoveDown = true;
+		//}
 
-		if (key == '=' || key == 61) 
-		{
-			CAMERA_STEP+=0.02;
-		}
-		if (key == '-' || key == 45) 
-		{
-			CAMERA_STEP-=0.02;
-		}
+		//if (key == '=' || key == 61) 
+		//{
+		//	CAMERA_STEP+=0.02;
+		//}
+		//if (key == '-' || key == 45) 
+		//{
+		//	CAMERA_STEP-=0.02;
+		//}
 
 	//	cout << (int)key << endl;
-		glutPostRedisplay();
+		//glutPostRedisplay();
 	}
 	void keybordUp(unsigned char key, int x, int y)
 	{
-		if (key == 'w' || key == 246) 
-		{
-			MoveForward = false;
-		}
+		///*if (key == 'w' || key == 246) 
+		//{
+		//	MoveForward = false;
+		//}
 
-		if (key == 's' || key == 251) 
-		{
-			MoveBack = false;
-		}
+		//if (key == 's' || key == 251) 
+		//{
+		//	MoveBack = false;
+		//}
 
-		if (key == 'a' || key == 244) 
-		{
-			RotateLeft = false;
-		}
+		//if (key == 'a' || key == 244) 
+		//{
+		//	RotateLeft = false;
+		//}
 
-		if (key == 'd' || key == 226)
-		{
-			RotateRight = false;
-		}
+		//if (key == 'd' || key == 226)
+		//{
+		//	RotateRight = false;
+		//}
 
-		if (key == 'r' || key == 234)
-		{
-			MoveUp = false;
-		}
-		if (key == 'f' || key == 224) 
-		{
-			MoveDown = false;
-		}
+		//if (key == 'r' || key == 234)
+		//{
+		//	MoveUp = false;
+		//}
+		//if (key == 'f' || key == 224) 
+		//{
+		//	MoveDown = false;
+		//}*/
 	}
 
 	void DrawTeapots()
 	{
 		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, colorTeapot);
-  
-		glPushMatrix();
-		float pos[4] = {25.0, 20.0, 25.0, 0.6};
-		glLightfv(GL_LIGHT0, GL_POSITION, pos);
-		glEnable(GL_LIGHT0);
-		glPopMatrix();
-
 		glNormal3d(0,1,0);
 
 		glPushMatrix();
@@ -401,22 +355,16 @@ namespace SourceScene {
 	}
 };
 
-namespace RestoredScene{
-	void idle(void)
-	{
-		glutPostRedisplay();
-	}
+namespace RestoredScene
+{	
+	void RenderFPS(int value);
 	void reshape(int w, int h)
 	{
-		WINDOW_WIDTH = w;
-		WINDOW_HEIGHT = h;
-		glViewport(0, 0, w, h);
-
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 	
 		double sw = 1.0;
-		double aspect = ((double)h)/w;
+		double aspect = ((double)h)/h;
 		double sh = sw * aspect;
 
 		glFrustum(-sw/2, sw/2, -sh/2, sh/2, Z_NEAR, Z_FAR);
@@ -426,12 +374,12 @@ namespace RestoredScene{
 	}
 	void display(void)
 	{
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glColor3d(1,1,1);
-
+		glViewport(W_WIDTH, 0, W_WIDTH, W_HEIGHT);
+		
 		glEnable(GL_DEPTH_TEST);
 		glPushMatrix();
 
+		glColor3d(1, 1, 1);
 		glBegin(GL_QUADS);
 		glVertex3d(-0.5, -0.5, zTest[2]);
 		glVertex3d(-0.5, 0.5, zTest[2]);
@@ -441,67 +389,80 @@ namespace RestoredScene{
 
 		glPopMatrix();
 		glDisable(GL_DEPTH_TEST);
-
+		RenderFPS(fps);
+		
 		glReadPixels(160, 120, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, depth); //GL_DEPTH_BITS = 24 bit per pixel
 		RestoreDepthFromBuffer(depth,1);
 	
+		/*
 		cout << "realVal=" << zTest[2]  << " "
 			 << "restoredVal=" << depth[0] << " "
 			 << endl;
 		zTest[2] = zTest[2] - 1;
-		system("Pause");
-		glFlush();
-		glutSwapBuffers();
+		*/
 	}
 	void keybord(unsigned char key, int x, int y)
 	{
-		if (key == '=')
+		/*if (key == '=')
 		{
 			zTest[2] -= 1;
 		}
 		if (key == '-')
 		{
 			zTest[2]+= 1;
-		}
+		}*/
 	
 		if (key == 'w' || key == 246) // Движение вперед
 		{
 			zTest[2] += 1;
-			cout << zTest[2] << endl;
+			//cout << zTest[2] << endl;
 			//w2camera.MoveForward(CAMERA_STEP);
 		}
 
 		if (key == 's' || key == 251) // Движение назад
 		{
 			zTest[2] -= 1;
-			cout << zTest[2] << endl;
+			//cout << zTest[2] << endl;
 			//w2camera.MoveBack(CAMERA_STEP);
 		}
 
-		if (key == 'a' || key == 244) // Поворот камеры направо
-		{
-			w2camera.Rotate(-CAMERA_STEP);
-		}
+		//if (key == 'a' || key == 244) // Поворот камеры направо
+		//{
+		//	w2camera.Rotate(-CAMERA_STEP);
+		//}
 
-		if (key == 'd' || key == 226) // Поворот камеры налево
-		{
-			w2camera.Rotate(CAMERA_STEP);
-		}
+		//if (key == 'd' || key == 226) // Поворот камеры налево
+		//{
+		//	w2camera.Rotate(CAMERA_STEP);
+		//}
 
 		glutPostRedisplay();
 	}
+	void RenderFPS(int value){
+		char buf[20];
+		double d = -0.5 + 0.04;
+
+		sprintf(buf,"FPS: %d", value);
+		
+		glColor3d(0,1,0);
+		glRasterPos3f (d, d, -3);
+		for(int i=0; buf[i]; i++) 
+		{
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, buf[i]);
+		}
+	};
 };	
 
 
 void DrawFPS(IplImage * pic, int _fps, int clusters)
 {
-	CvPoint pt = cvPoint( WINDOW_WIDTH-100, WINDOW_HEIGHT-30 );
+	CvPoint pt = cvPoint( W_WIDTH-100, W_HEIGHT-30 );
     CvFont font;
     cvInitFont( &font, CV_FONT_HERSHEY_SIMPLEX, 0.7, 0.7, 0, 0, CV_AA);
 	char buf[20];
 	sprintf(buf,"FPS: %d",_fps);
 	cvPutText(pic, buf, pt, &font, CV_RGB(100, 100, 255));
-	pt = cvPoint( 10, WINDOW_HEIGHT-30 );
+	pt = cvPoint( 10, W_HEIGHT-30 );
 	sprintf(buf,"CLUSTERS: %d", clusters);
 	cvPutText(pic, buf, pt, &font, CV_RGB(100, 100, 255));
 }
@@ -523,40 +484,67 @@ void SetConsole()
 	SetWindowPos(hwndFound, 0, 0, 350, 800, 300, 0);
 }
 
+void reshape(int w, int h){
+	W_WIDTH = w/2;
+	W_HEIGHT = h;
+	glutPostRedisplay();
+};
+void display(void){
+
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	long curTime = GetTickCount();
+	long dt = curTime - prevTime;
+	if(dt > 1000) 
+	{
+		fps = frameCount;
+		frameCount = 0; 
+		prevTime = curTime;
+	} 
+	else frameCount++;
+	
+		
+	SourceScene::reshape(W_WIDTH, W_HEIGHT); 
+	SourceScene::display();
+
+	RestoredScene::reshape(W_WIDTH, W_HEIGHT);
+	RestoredScene::display();
+
+	glFlush();
+	glutSwapBuffers();
+};
+void idle(void)
+{
+	glutPostRedisplay();
+}
+
 int main(int argc, char **argv)
 {
 	setlocale(LC_ALL, "RUS");
 	SetConsole(); //this item does not work!
 	
 	//create buffers
-	CvSize s = cvSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	CvSize s = cvSize(W_WIDTH, W_HEIGHT);
 	img = cvCreateImage(s, IPL_DEPTH_8U, 3);
 	gray = cvCreateImage(s, IPL_DEPTH_8U, 1);
 	dst = cvCreateImage(s, IPL_DEPTH_8U, 1);
-	depth = new float[WINDOW_WIDTH * WINDOW_HEIGHT];
-	mainmas = new float[WINDOW_WIDTH * WINDOW_HEIGHT * 3];
+	img->origin = IPL_ORIGIN_BL;
+	gray->origin = IPL_ORIGIN_BL;
+	dst->origin = IPL_ORIGIN_BL;
+	depth = new float[W_WIDTH * W_HEIGHT];
+	mainmas = new float[W_WIDTH * W_HEIGHT * 3];
 
 	//init OpenGL
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE |  GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	
-	/*
-	glutInitWindowPosition(850,0);
-	w1 = glutCreateWindow("OpenGL - Имитация реального мира");
-	glutIdleFunc(SourceScene::idle);
-	glutDisplayFunc(SourceScene::display);
-	glutReshapeFunc(SourceScene::reshape);
-	glutKeyboardFunc(SourceScene::keybord);
-	glutKeyboardUpFunc(SourceScene::keybordUp);
-	*/
-	
-	glutInitWindowPosition(100, 750);
-	w2 = glutCreateWindow("OpenGL - Восстановление трёхмерной сцены");
-	glutSetWindow(w2);
-	glutIdleFunc(RestoredScene::idle);
-	glutReshapeFunc(RestoredScene::reshape);
-	glutDisplayFunc(RestoredScene::display);
+	glutInitWindowSize(2 * W_WIDTH, W_HEIGHT);
+
+	glutInitWindowPosition(50, 500);
+	glutCreateWindow("Восстановление трёхмерной сцены");
+	glutIdleFunc(idle);
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
 	glutKeyboardFunc(RestoredScene::keybord);
 	
 	glutMainLoop();
--- OpenCV/scene.h
+++
@@ -1,6 +0,0 @@
-#include "glut.h"
-
-#ifndef __SOURCE_SCENE_H
-#define __SOURCE_SCENE_H
-
-#endif
